6d589da69ff0efd81be97815ea59f70921f6d94e
DeymarHA <krn100dha.dha@gmail.com>
2024-06-17 12:25:01 -0400

    estoy cansado jefe

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.get('/', (req, res)=>{
     res.status(200).send('hola a mi bot deymar')
 })
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
     console.log('Servidor iniciado en el puerto ${PORT}');
 });
