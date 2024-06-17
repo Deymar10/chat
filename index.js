90622af553ec1eaffa2a22d3495f1eff5571be4f
DeymarHA <krn100dha.dha@gmail.com>
2024-06-17 11:04:15 -0400

    agregado de constante PORT

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.get('/', (req, res)=>{
     res.status(200).send('hola a mi bot deymar')
 })
 
-app.listen(8000, () =>{
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () =>{
     console.log('Servidor iniciado...');
 });
