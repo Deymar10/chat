3f70d77abcf115d77dbec7e430a27b6cf8c79aa7
DeymarHA <krn100dha.dha@gmail.com>
2024-06-17 18:27:30 -0400

    servidor corregido

--- index.js
+++ index.js
@@ -49,5 +49,5 @@ app.get('/', (req, res)=>{
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
-    console.log('Servidor iniciado en el puerto ${PORT}');
+    console.log('Servidor iniciado');
 });
